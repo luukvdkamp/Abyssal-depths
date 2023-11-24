be3285e60bca783a83d9c434871e7f4ca0592386
LusseveldNiek <97088465@st.deltion.nl>
2023-11-24 16:00:42 +0100

    gun aiming

--- Assets/programming/Scripts/Gun.cs
+++ Assets/programming/Scripts/Gun.cs
@@ -8,6 +8,9 @@ public class Gun : MonoBehaviour
 
     private void Update()
     {
-        transform.LookAt(mousePosition);
+        float angle = Mathf.Atan2(mousePosition.position.y - transform.position.y, mousePosition.position.x - transform.position.x) * Mathf.Rad2Deg;
+
+        // Rotate the gun
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
     }
 }
