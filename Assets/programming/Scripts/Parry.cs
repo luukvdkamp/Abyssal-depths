35ae98ba1707f30797b7fc4e464ffcfecebce6c6
LusseveldNiek <97088465@st.deltion.nl>
2023-11-25 15:09:30 +0100

    parry

---
+++ Assets/programming/Scripts/Parry.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parry : MonoBehaviour
+{
+    public GameObject gun;
+    public GameObject knife;
+
+    private bool resetParry;
+    public float maxParryTime;
+    private float parryCount;
+
+    private bool parryCooldown;
+    public float maxParryCooldown;
+    private float parryCooldownCount;
+
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire2") && parryCooldown == false)
+        {
+            knife.SetActive(true);
+            gun.SetActive(false);
+
+            resetParry = true;
+        }
+
+        //decide the time the parry lasts
+        if(resetParry)
+        {
+            parryCount += Time.deltaTime;
+            if(parryCount > maxParryTime)
+            {
+                resetParry = false;
+                parryCount = 0;
+
+                knife.SetActive(false);
+                gun.SetActive(true);
+
+                //give parry cooldown
+                parryCooldown = true;
+            }
+        }
+
+        //cooldown
+        if(parryCooldown)
+        {
+            parryCooldownCount += Time.deltaTime;
+            if(parryCooldownCount > maxParryCooldown)
+            {
+                parryCooldownCount = 0;
+                parryCooldown = false;
+            }
+        }
+    }
+}
