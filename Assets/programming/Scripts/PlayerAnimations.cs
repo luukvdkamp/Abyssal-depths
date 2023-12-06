b42bbdd7b88d897a3f4f7fdce4942803805d45e5
LusseveldNiek <97088465@st.deltion.nl>
2023-12-06 20:33:19 +0100

    player controller

--- Assets/programming/Scripts/PlayerAnimations.cs
+++ Assets/programming/Scripts/PlayerAnimations.cs
@@ -8,6 +8,7 @@ public class PlayerAnimations : MonoBehaviour
     public WallJumping wallJumping;
     public Health health;
     public Bow bow;
+    public Gun gun;
 
     public Animator animator;
 
@@ -20,16 +21,27 @@ public class PlayerAnimations : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //walking
         float walkSpeed = Input.GetAxis("Horizontal");
         if(walkSpeed != 0 && movementPlayer.isGrounded)
         {
             animator.SetBool("Walking", true);
-            animator.speed = walkSpeed;
         }
 
         else
         {
             animator.SetBool("Walking", false);
+        }
+
+        //rotate mouse aim
+        if(gun.targetPosition.position.x > transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
         }
     }
 }
