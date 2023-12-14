c408c7b854e0801daddb282e97ca7acb543f3215
LusseveldNiek <97088465@st.deltion.nl>
2023-12-14 16:10:17 +0100

    resolution

---
+++ Assets/programming/Scripts/UI/ResolutionUI.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class ResolutionUI : MonoBehaviour
+{
+    private TMP_Dropdown resolutionDropDown;
+
+    private Resolution[] resolutions;
+    private List<Resolution> filteredResolutions;
+
+    private float currentRefreshRate;
+    private int currentResolutionIndex = 0;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        filteredResolutions = new List<Resolution>();
+
+        resolutionDropDown.ClearOptions();
+        currentRefreshRate = Screen.currentResolution.refreshRate;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            if(resolutions[i].refreshRate == currentRefreshRate)
+            {
+                filteredResolutions.Add(resolutions[i]);
+            }
+        }
+
+        List<string> options = new List<string>();
+        for(int i = 0; i < filteredResolutions.Count; i++)
+        {
+            string resolutionOption = filteredResolutions[i].width + "x" + filteredResolutions[i].height + "x" + filteredResolutions[i].refreshRate + "Hz";
+            options.Add(resolutionOption);
+            if(filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = filteredResolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
+    }
+}
