df5fa111d7639766c5748368f20e102d352556aa
LusseveldNiek <97088465@st.deltion.nl>
2024-01-24 17:00:29 +0100

    door animation

--- Assets/programming/Scripts/Lever/LeverTrigger.cs
+++ Assets/programming/Scripts/Lever/LeverTrigger.cs
@@ -7,32 +7,66 @@ public class LeverTrigger : MonoBehaviour
     public GameObject[] doors;
     public Animator animator;
     public AudioSource leverPull;
+    private bool doorOpen;
+
+    // Time it takes for the doors to move (1 second in this case)
+    public float doorMoveDuration = 1f;
+
+    // Distance the doors should move (adjust as needed)
+    public float doorMoveDistance;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        doorOpen = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.gameObject.tag == "Player")
+        if (other.transform.gameObject.tag == "Player")
         {
-            for (int i = 0; i < doors.Length; i++)
-            {
-                if(doors[i].activeInHierarchy)
-                {
-                    doors[i].gameObject.SetActive(false);
-                    animator.SetBool("leverPulled", true);
-                }
-
-                else
-                {
-                    doors[i].gameObject.SetActive(true);
-                    animator.SetBool("leverPulled", false);
-                }
-            }
+            StartCoroutine(MoveDoors());
             leverPull.Play();
+        }
+    }
+
+    IEnumerator MoveDoors()
+    {
+        if (doorOpen == false)
+        {
+            animator.SetBool("leverPulled", true);
+            doorOpen = true;
+
+            // Move doors up
+            float initialHeight = doors[0].transform.position.y;
+            float targetHeight = initialHeight + doorMoveDistance;
+
+            for (float t = 0; t < 1f; t += Time.deltaTime / doorMoveDuration)
+            {
+                for (int i = 0; i < doors.Length; i++)
+                {
+                    doors[i].transform.Translate(Vector3.up * doorMoveDistance * Time.deltaTime / doorMoveDuration);
+                }
+                yield return null;
+            }
+        }
+        else
+        {
+            animator.SetBool("leverPulled", false);
+            doorOpen = false;
+
+            // Move doors down
+            float initialHeight = doors[0].transform.position.y;
+            float targetHeight = initialHeight - doorMoveDistance;
+
+            for (float t = 0; t < 1f; t += Time.deltaTime / doorMoveDuration)
+            {
+                for (int i = 0; i < doors.Length; i++)
+                {
+                    doors[i].transform.Translate(Vector3.down * doorMoveDistance * Time.deltaTime / doorMoveDuration);
+                }
+                yield return null;
+            }
         }
     }
 }
